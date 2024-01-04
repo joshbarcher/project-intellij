7aaeb7fdc7f679c2429aa8e61d6c95d7709e0b31
Josh Archer <jarcher@greenriver.edu>
2024-01-03 16:46:07 -0800

    Revert "Refactored my Main class."
    This reverts commit 2bd0a88c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        printHeader();
-        printMessage("Hello, world!");
-
-        printHeader();
-        printMessage("By: Josh");
-    }
-
-    private static void printMessage(String message)
-    {
-        System.out.println(message);
-    }
-
-    private static void printHeader()
-    {
-        System.out.println("**********************************");
+        System.out.println("Hello, world!");
+        System.out.println("By: Josh");
     }
 }
