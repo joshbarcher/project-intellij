a451fee33871bb9f5824f06d469e955761056242
Josh Archer <jarcher@greenriver.edu>
2024-01-03 16:43:10 -0800

    Added a method to print a header.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,20 @@ public class Main
 {
     public static void main(String[] args)
     {
+        printHeader();
         printMessage("Hello, world!");
+
+        printHeader();
         printMessage("By: Josh");
     }
 
     private static void printMessage(String message)
     {
         System.out.println(message);
+    }
+
+    private static void printHeader()
+    {
+        System.out.println("**********************************");
     }
 }
